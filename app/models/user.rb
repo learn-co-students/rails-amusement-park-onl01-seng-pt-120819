3196bfeee02fcbed76e896fd0edf092c2e2d8899
RebeccaHA <rebeccahunt034@gmail.com>
2020-05-27 18:53:52 +0100

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
     has_many :attractions, through: :rides
 
     has_secure_password
+
+    def mood
+        if happiness > nausea
+            "happy"
+        else
+            "sad"
+        end
+    end
 end
